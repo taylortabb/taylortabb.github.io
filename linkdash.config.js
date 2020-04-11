0ddd5b7a50734ae3cd2313601800d466b40c1076
“Taylor <taytabbbb@aol.com>
2020-04-11 15:37:17 -0400

    introducing: linkdash

---
+++ linkdash.config.js
@@ -0,0 +1,245 @@
+module.exports = async () => {
+  return {
+    title: "Welcome to my linkdash!",
+
+    // a static list of urls to build the linkdash list from
+    urls: [
+      {
+        title: "Open Flip",
+        href: "http://flip.tabb.me/",
+        group: "FUN",
+
+        // optional attributes
+        keywords: "flip, tabb.me, tabb",
+        id: "flip",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+            {
+        title: "Bubble Dashboard",
+        href: "http://bubblesrightnow.com/",
+        group: "FUN",
+
+        // optional attributes
+        keywords: "bubbles right now, pittsbubble, bubble, bubbles",
+        id: "bubble",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+            {
+        title: "Open Me, As A Clock",
+        href: "http://measaclock.xyz/",
+        group: "FUN",
+
+        // optional attributes
+        keywords: "clock, time",
+        id: "clock",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+            {
+        title: "View Scrubber Documentation",
+        href: "https://github.com/Deeplocal/scrubber",
+        group: "DPLCL",
+
+        // optional attributes
+        keywords: "scrubber, soap, hand, wash",
+        id: "scrub",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+            {
+        title: "View Booth Move On Timer",
+        href: "http://whenismoveon.com/",
+        group: "SIGEP",
+
+        // optional attributes
+        keywords: "Booth, sigep, cmu",
+        id: "move-on-timer",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+            {
+        title: "View Portfolio Workshop",
+        href: "http://go.tabb.me/buildaportfolio/",
+        group: "TUTORIAL",
+
+        // optional attributes
+        keywords: "Portfolio, workshop, sigep",
+        id: "portfolio-workshop",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+            {
+        title: "Sigep CMU Website",
+        href: "http://sigeppenntheta.github.io",
+        group: "SIGEP",
+
+        // optional attributes
+        keywords: "cmu, sigep",
+        id: "sigep-cmu",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+            {
+        title: "Sigep Shop",
+        href: "http://shop.sigep.fun",
+        group: "SIGEP",
+
+        // optional attributes
+        keywords: "sigep, cmu, shop",
+        id: "sigep-shop",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+            {
+        title: "View 3rd Floor Laundry Status",
+        href: "https://www.laundryview.com/home/7289/4997158/CARNEGIE-MELLON-UNIVERSITY/GREEK-QUAD-1057-2ND-FLOOR",
+        group: "SIGEP",
+
+        // optional attributes
+        keywords: "sigep, cmu, laundry",
+        id: "sigep-laundry",
+        isBookmarked: false, // bookmarks the link by default
+      },
+            {
+        title: "View Weekly Menu",
+        href: "https://www.sigep.fun/menu/",
+        group: "SIGEP",
+
+        // optional attributes
+        keywords: "sigep, cmu, menu, food",
+        id: "sigep-menu",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+                  {
+        title: "Pi-Cam Travel Blog",
+        href: "http://go.tabb.me/travelblogimages/",
+        group: "FUN",
+
+        // optional attributes
+        keywords: "raspberry pi, camera, photo, travel",
+        id: "fun-pi",
+        isBookmarked: false, // bookmarks the link by default
+      },
+                  {
+        title: "Read ModiFiber Paper",
+        href: "https://dl.acm.org/doi/10.1145/3290605.3300890",
+        group: "CMU",
+
+        // optional attributes
+        keywords: "morphing, matter, lab, research, modifiber, jack, forman",
+        id: "mml-modifiber-paper",
+        isBookmarked: false, // bookmarks the link by default
+      },
+                  {
+        title: "Link Shortener",
+        href: "http://tt.vu/",
+        group: "FUN",
+
+        // optional attributes
+        keywords: "tt.vu, link, Shortener, short",
+        id: "tt-vu",
+        isBookmarked: false, // bookmarks the link by default
+      },
+                  {
+        title: "Use Minima Website Generator",
+        href: "http://minima.tabb.me/",
+        group: "TUTORIAL",
+
+        // optional attributes
+        keywords: "website, portfolio, TUTORIAL",
+        id: "minima-generator",
+        isBookmarked: false, // bookmarks the link by default
+      },
+                  {
+        title: "Launch Buggy App",
+        href: "https://www.sigep.fun/push",
+        group: "SIGEP",
+
+        // optional attributes
+        keywords: "sigep, buggy, cmu",
+        id: "sigep-buggy",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+                        {
+        title: "Launch Booth App",
+        href: "https://www.sigep.fun/build",
+        group: "SIGEP",
+
+        // optional attributes
+        keywords: "sigep, booth, cmu",
+        id: "sigep-booth",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+                              {
+        title: "Cheeseborg Info",
+        href: "https://www.tabb.me/grilledcheese/",
+        group: "FUN",
+
+        // optional attributes
+        keywords: "cheeseborg, grilled cheese, robot cmu",
+        id: "cheeseborg",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+                              {
+        title: "View ModiFiber Documentation",
+        href: "https://www.tabb.me/morphing-matter/",
+        group: "CMU",
+
+        // optional attributes
+        keywords: "Modifiber, cmu, morphing, matter, lab, jack, forman",
+        id: "modifiber-doc",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+                              {
+        title: "Read About Hybrid Fruits",
+        href: "https://indd.adobe.com/view/100cf9fb-a708-470e-9fdf-a9b4a2fde59d",
+        group: "CMU",
+
+        // optional attributes
+        keywords: "design, fruit, hybrid, cmu",
+        id: "hybrid-fruit",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+                                    {
+        title: "Set Up Raspberry Pi On CMU WiFi",
+        href: "https://www.tabb.me/thinking/cmupisetup",
+        group: "TUTORIAL",
+
+        // optional attributes
+        keywords: "cmu, pi, raspberry, tutorial",
+        id: "rpi-cmu",
+        isBookmarked: false, // bookmarks the link by default
+      },
+
+                                    {
+        title: "Embed Google Calendar In Squarespace Without Premium",
+        href: "https://www.tabb.me/thinking/2019/3/27/how-to-embed-google-calendar-in-squarespace-without-a-premium-plan",
+        group: "TUTORIAL",
+
+        // optional attributes
+        keywords: "squarespace, calendar, tutorial",
+        id: "calendar-sspace",
+        isBookmarked: false, // bookmarks the link by default
+      },
+    ],
+    // [OR]
+    // specifiy a host to grab the config from
+    // host: "https://linkdash.now.sh/api/demo-config",
+
+    disableOpen: false,
+    output: "./linkdash.html",
+    htmlHead: `
+    <meta name="robots" content="noindex" />
+    `.trim(),
+    // automatically opens the menu
+    enableAutoMenu: false,
+  };
+};
